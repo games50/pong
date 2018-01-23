a7f39702d9f16f16cd1ca360d03b1cc56c504462
Colton Ogden <coltonoscopy@gmail.com>
2018-01-23 05:46:50 -0500

    pong-5

---
+++ pong-5/Ball.lua
@@ -0,0 +1,50 @@
+--[[
+    GD50 2018
+    Pong Remake
+
+    -- Ball Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents a ball which will bounce back and forth between paddles
+    and walls until it passes a left or right boundary of the screen,
+    scoring a point for the opponent.
+]]
+
+Ball = Class{}
+
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    -- these variables are for keeping track of our velocity on both the
+    -- X and Y axis, since the ball can move in two dimensions
+    self.dy = math.random(2) == 1 and -100 or 100
+    self.dx = math.random(-50, 50)
+end
+
+--[[
+    Places the ball in the middle of the screen, with an initial random velocity
+    on both axes.
+]]
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dy = math.random(2) == 1 and -100 or 100
+    self.dx = math.random(-50, 50)
+end
+
+--[[
+    Simply applies velocity to position, scaled by deltaTime.
+]]
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
