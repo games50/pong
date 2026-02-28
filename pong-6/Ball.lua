646db5a4c356b2fe64a9a170c86e582e2928dbc2
Chad Whitely <chadwhitely@gmail.com>
2026-02-28 15:35:55 -0700

    Fix the flipped dx and dy which changed with pong-5's refactor

--- pong-6/Ball.lua
+++ pong-6/Ball.lua
@@ -22,8 +22,8 @@ function Ball:init(x, y, width, height)
 
     -- these variables are for keeping track of our velocity on both the
     -- X and Y axis, since the ball can move in two dimensions
-    self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50)
 end
 
 --[[
@@ -54,8 +54,8 @@ end
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 - 2
-    self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50)
 end
 
 --[[
